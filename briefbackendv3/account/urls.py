35006c24d9650e477606c8e49b0c8e397ae0e7aa
srir4m <112010096+srir4m@users.noreply.github.com>
2023-08-02 12:10:22 -0400

    token validation

--- briefbackendv3/account/urls.py
+++ briefbackendv3/account/urls.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('validate_token/', views.validate_token, name='validate_token'),
     
     path("login/", views.login_user, name="login_user"),
     path("logout_user/", views.logout_user, name="logout_user"),
