2906d76e9cd7b1f8edf2b92cd05bb09a8a2b88ba
Pekay <github@maxlingenfelter.com>
2023-08-07 16:06:24 -0400

    feat: Wokring script

--- briefbackendv3/news/management/commands/populate_database.py
+++ briefbackendv3/news/management/commands/populate_database.py
@@ -1,4 +1,5 @@
-import requests
+import random
+import time
 from datetime import datetime, timedelta
 from django.core.management.base import BaseCommand
 from news.models import Article
@@ -7,35 +8,38 @@ class Command(BaseCommand):
     help = 'Fetches latest news articles from mediastack API and saves them to the database'
 
     def handle(self, *args, **options):
-        api_key = 'f11cf661ab144dcd5295c1261835846c'
-        start_date = datetime.now() - timedelta(days=300)
-        end_date = datetime.now()
-        params = {
-            'access_key': api_key,
-            'languages': 'en',
-            'sort': 'published_desc',
-            'limit': 100,
-            'offset': 0,
-            'date_from': start_date.strftime('%Y-%m-%d'),
-            'date_to': end_date.strftime('%Y-%m-%d'),
-        }
-        total_results = 0
-        while True:
-            response = requests.get('http://api.mediastack.com/v1/news', params=params)
-            data = response.json()
-            if 'data' not in data:
-                break
-            for article in data['data']:
-                Article.objects.create(
-                    title=article['title'],
-                    description=article['description'],
-                    url=article['url'],
-                    published_at=datetime.fromisoformat(article['published_at']),
-                    language = article['language'],
-                    image = article['image'],
-                    source = article['source'],
-                )
-            total_results += len(data['data'])
-            params['offset'] += 100
-            if total_results >= 1000:
-                break
+        api_key = 'xxx'
+
+        for month_offset in range(10):
+            end_date = datetime.now() - timedelta(days=30 * month_offset)
+            start_date = end_date - timedelta(days=30)
+
+            print(f"Date Range: {start_date.strftime('%Y-%m-%d')} - {end_date.strftime('%Y-%m-%d')}")
+
+            for _ in range(10):
+                random_day = random.randint(1, 30)
+                random_date = start_date + timedelta(days=random_day)
+
+                # Calculate an offset in hours for more data variety
+                offset_hours = random.randint(0, 23)
+                random_date += timedelta(hours=offset_hours)
+
+                params = {
+                    'access_key': api_key,
+                    'languages': 'en',
+                    'sort': 'published_desc',
+                    'limit': 100,
+                    'offset': 0,
+                    'date_from': random_date.strftime('%Y-%m-%d'),
+                    'date_to': (random_date + timedelta(days=1)).strftime('%Y-%m-%d'),
+                }
+
+                url = 'http://api.mediastack.com/v1/news?' + '&'.join([f'{k}={v}' for k, v in params.items()])
+                print(url)
+
+                # Rest of your code for API calls and processing
+
+                # Add a delay between API calls to spread them out
+                time.sleep(10)  # Adjust the delay time as needed
+
+# ...
