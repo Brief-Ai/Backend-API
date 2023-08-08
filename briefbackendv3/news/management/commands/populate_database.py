7edc1c2e1982a0e9c7c79431abde582602506d75
srir4m <112010096+srir4m@users.noreply.github.com>
2023-08-08 11:09:06 -0400

    new db for training & updated search engine

--- briefbackendv3/news/management/commands/populate_database.py
+++ briefbackendv3/news/management/commands/populate_database.py
@@ -1,41 +1,52 @@
 import requests
+import time
 from datetime import datetime, timedelta
 from django.core.management.base import BaseCommand
 from news.models import Article
 
 class Command(BaseCommand):
-    help = 'Fetches latest news articles from mediastack API and saves them to the database'
+    help = 'Fetches and adds 3000 latest news articles from mediastack API to the database'
 
     def handle(self, *args, **options):
-        api_key = 'f11cf661ab144dcd5295c1261835846c'
-        start_date = datetime.now() - timedelta(days=300)
+        api_key = 'cf16d9738a9525f1525c1667848c7391'
+        articles_to_fetch = 3000
+        fetched_articles = 0
         end_date = datetime.now()
-        params = {
-            'access_key': api_key,
-            'languages': 'en',
-            'sort': 'published_desc',
-            'limit': 100,
-            'offset': 0,
-            'date_from': start_date.strftime('%Y-%m-%d'),
-            'date_to': end_date.strftime('%Y-%m-%d'),
-        }
-        total_results = 0
-        while True:
-            response = requests.get('http://api.mediastack.com/v1/news', params=params)
-            data = response.json()
-            if 'data' not in data:
-                break
-            for article in data['data']:
-                Article.objects.create(
-                    title=article['title'],
-                    description=article['description'],
-                    url=article['url'],
-                    published_at=datetime.fromisoformat(article['published_at']),
-                    language = article['language'],
-                    image = article['image'],
-                    source = article['source'],
-                )
-            total_results += len(data['data'])
-            params['offset'] += 100
-            if total_results >= 1000:
-                break
+
+        while fetched_articles < articles_to_fetch:
+            start_date = end_date - timedelta(days=30)
+
+            while start_date <= end_date and fetched_articles < articles_to_fetch:
+                params = {
+                    'access_key': api_key,
+                    'languages': 'en',
+                    'sort': 'published_desc',
+                    'limit': min(100, articles_to_fetch - fetched_articles),
+                    'offset': 0,
+                    'date_from': start_date.strftime('%Y-%m-%d'),
+                    'date_to': start_date.strftime('%Y-%m-%d'),
+                }
+
+                response = requests.get('http://api.mediastack.com/v1/news', params=params)
+                data = response.json()
+                if 'data' not in data:
+                    break
+                for article in data['data']:
+                    Article.objects.create(
+                        title=article['title'],
+                        description=article['description'],
+                        url=article['url'],
+                        published_at=datetime.fromisoformat(article['published_at']),
+                        language=article['language'],
+                        image=article['image'],
+                        source=article['source'],
+                    )
+                    fetched_articles += 1
+                params['offset'] += 100
+
+                # Add a delay between API calls to spread them out
+                time.sleep(10)  # Adjust the delay time as needed
+
+                start_date += timedelta(days=1)
+
+            end_date -= timedelta(days=30)
