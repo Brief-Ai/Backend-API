6dffd5d9639bb9933bc3466f21d7ffdd65d7eca0
Pekay <github@maxlingenfelter.com>
2023-08-08 11:10:27 -0400

    Update urls.py

--- briefbackendv3/news/urls.py
+++ briefbackendv3/news/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import NewsArticleSearchView
+from .views import NewsArticleRecommendedView
 
 urlpatterns = [
     path('search/', NewsArticleSearchView.as_view(), name='news_article_search'),
+    
+    # Add route for recommended articles
+    path('recommended/', NewsArticleRecommendedView.as_view(), name='news_article_recommended'),
 ]
